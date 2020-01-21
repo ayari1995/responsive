9ee657e70ebf5b9e36b2abc7f5898c5433aee504
ayari1995 <ayariachref1995@gmail.com>
2020-01-21 15:21:08 +0100

    Transition Backgrounds

--- app.js
+++ app.js
@@ -39,3 +39,12 @@ function imageGallery() {
 }
 
 imageGallery();
+
+function bgChanger() {
+  if (this.scrollY > this.innerHeight / 2) {
+    document.body.classList.add('bg-active');
+  } else {
+    document.body.classList.remove('bg-active');
+  }
+}
+window.addEventListener('scroll', bgChanger);
